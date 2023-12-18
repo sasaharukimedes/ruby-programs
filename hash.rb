a2dcae5ee11b8e8904c9a7d58eeb15369f35cbf7
sasaharukimedes <sasaharu3@gmail.com>
2023-12-18 22:48:52 +0900

    add hash

---
+++ hash.rb
@@ -0,0 +1,24 @@
+# Rubyでハッシュ（Hash）を初期化する方法はいくつかあります。以下はいくつかの一般的な方法です。
+
+# 1. **ハッシュリテラルを使用する:**
+
+  my_hash = { key1: 'value1', key2: 'value2' }
+
+
+# 2. **`Hash.new` コンストラクタを使用する:**
+  my_hash = Hash.new
+  my_hash[:key1] = 'value1'
+  my_hash[:key2] = 'value2'
+
+
+# 3. **`Hash` クラスの `[]` メソッドを使用する:**
+
+  my_hash = Hash[:key1, 'value1', :key2, 'value2']
+
+
+# 4. **ブロックを使用して `Hash.new` を初期化する:**
+  my_hash = Hash.new { |hash, key| hash[key] = "Default value for #{key}" }
+
+この方法は、ハッシュに存在しないキーにアクセスした場合に、そのキーに対するデフォルト値を設定します。
+
+これらの方法の選択は、具体的な要件やコードの読みやすさによります。ルビーではハッシュリテラルが一般的に使われ、コードが簡潔で読みやすくなります。
